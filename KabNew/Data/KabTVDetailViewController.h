39903a0662837e75c95245529fe55265996891d8
Gabe Morales <gabomorales@me.com>
2014-12-27 22:25:58 -0600

    Model for Tv.Kab view

--- KabNew/Data/KabTVDetailViewController.h
+++ KabNew/Data/KabTVDetailViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+@class KabChannelSchema;
 
-@interface KabTVDetailViewController : UIViewController
+@interface KabTVDetailViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic) NSArray *list;
+@property (nonatomic) KabChannelSchema *channel;
+@property (nonatomic, readonly) UIImageView *backgroundView;
+@property (nonatomic, readonly) UILabel *backgroundTitle;
+@property (nonatomic, readonly) UIImageView *titleBackgroundView;
+@property (nonatomic, readonly) UITableView *table;
+
+
 
 @end
--- KabNew/Data/KabTVDetailViewController.m
+++ KabNew/Data/KabTVDetailViewController.m
@@ -9,13 +9,36 @@
 #import "KabTVDetailViewController.h"
 
 @interface KabTVDetailViewController ()
-
+@property (nonatomic, readonly) NSDictionary *model;
 @end
 
 @implementation KabTVDetailViewController
+@synthesize backgroundView = _backgroundView;
+@synthesize backgroundTitle = _backgroundTitle;
+@synthesize titleBackgroundView = _titleBackgroundView;
+@synthesize table = _table;
+
+- (UITableView *)table {
+    if (!_table) {
+        _table = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStyleGrouped];
+        _table.delegate = self;
+        _table.dataSource = self;
+        _table.backgroundColor = [UIColor kabStaticColor];
+        _table.scrollsToTop = YES;
+        _table.scrollEnabled = YES;
+        [_table registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+        [_table reloadData];
+        _table.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+        _table.separatorColor = [UIColor kabBlueColor];
+    }
+    return _table;
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view addSubview:self.table];
+    [self layoutConstraints];
     // Do any additional setup after loading the view.
 }
 
@@ -24,14 +47,43 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - UITableView
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSArray *cellData = self.model[@"courses"];
+    return cellData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [_table dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.backgroundColor = [UIColor kabStaticColor];
+    }
+    
+    cell.textLabel.text = [self.model[@"courses"] objectAtIndex:indexPath.row][@"courseTitle"];
+    cell.detailTextLabel.text = [self.model[@"courses"] objectAtIndex:indexPath.row][@"description"];
+    return cell;
+    
+}
+
+#pragma mark - Configuration
+- (void)layoutConstraints {
+    NSDictionary *viewDict = @{@"table": self.table};
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[table]|"
+                                                                      options:kNilOptions metrics:nil
+                                                                        views:viewDict]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[table]|"
+                                                                      options:kNilOptions metrics:nil
+                                                                        views:viewDict]];
+}
+
 
 @end
