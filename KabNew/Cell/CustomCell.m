4de3a837851e70c7065001f56b48d3c02bac53cc
Gabe Morales <gmorales@phunware.com>
2015-12-04 09:02:27 -0600

    Updated constraints

--- KabNew/Cell/CustomCell.m
+++ KabNew/Cell/CustomCell.m
@@ -7,7 +7,6 @@
 //
 
 #import "CustomCell.h"
-#import "KabChannelSchema.h"
 
 @implementation CustomCell
 @synthesize titleLabel = _titleLabel;
@@ -21,43 +20,21 @@
         [self addSubview:self.subtitleLabel];
         [self addSubview:self.cellImageView];
         
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.cellImageView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.cellImageView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.0]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.cellImageView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeWidth multiplier:0.0 constant:90]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.cellImageView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeHeight multiplier:0.0 constant:85]];
         
-        NSDictionary *viewDict = @{@"titleLabel": _titleLabel,
-                                   @"subtitleLabel": _subtitleLabel,
-                                   @"cellImage": _cellImageView};
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.titleLabel attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.cellImageView attribute:NSLayoutAttributeRight multiplier:1.0 constant:8.0]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.titleLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:-3.0]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.titleLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:10]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.titleLabel attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeHeight multiplier:0.0 constant:20]];
         
-        //Title
-        NSArray *title_H = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-95-[titleLabel]"
-                                                                        options:0 metrics:nil
-                                                                          views:viewDict];
-        
-        NSArray *title_V = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[titleLabel(18)]"
-                                                                   options:0 metrics:nil
-                                                                     views:viewDict];
-        
-        //Subtitle
-        NSArray *subtitle_H = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-95-[subtitleLabel]-|"
-                                                                      options:0 metrics:nil
-                                                                        views:viewDict];
-        NSArray *subtitle_V = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-19-[subtitleLabel(70)]"
-                                                                      options:0 metrics:nil
-                                                                        views:viewDict];
-        
-        //Cell Image
-        NSArray *image_H = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-6-[cellImage(80)]"
-                                                                   options:0 metrics:nil
-                                                                     views:viewDict];
-        
-        NSArray *image_V = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-13-[cellImage(65)]"
-                                                                   options:0 metrics:nil
-                                                                     views:viewDict];
-        
-        [self addConstraints:title_H];
-        [self addConstraints:title_V];
-        [self addConstraints:subtitle_H];
-        [self addConstraints:subtitle_V];
-        [self addConstraints:image_H];
-        [self addConstraints:image_V];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.subtitleLabel attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.cellImageView attribute:NSLayoutAttributeRight multiplier:1.0 constant:10]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.subtitleLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:-10]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.subtitleLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.titleLabel attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.subtitleLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-5]];
+//        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.subtitleLabel attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeHeight multiplier:0.0 constant:70]];
     }
     return self;
 }
@@ -65,7 +42,7 @@
 #pragma mark - UIControls
 - (UILabel *)titleLabel {
     if (!_titleLabel) {
-        _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(95.0f, 10.0f, 218.0f, 18.0f)];
+        _titleLabel = [[UILabel alloc] init];
         _titleLabel.textColor = [UIColor kabBlueColor];
         _titleLabel.font = [UIFont boldSystemFontOfSize:UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 30.0 : 14.0];
         _titleLabel.textAlignment = NSTextAlignmentLeft;
@@ -78,9 +55,9 @@
 
 - (UILabel *)subtitleLabel {
     if (!_subtitleLabel) {
-        _subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(95.0f, 19.0f, 218.0f, 70.0f)];
+        _subtitleLabel = [[UILabel alloc] init];
         _subtitleLabel.textColor = [UIColor lightGrayColor];
-        _subtitleLabel.font = [UIFont systemFontOfSize:UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 24.0 : 12.0];
+        _subtitleLabel.font = [UIFont systemFontOfSize:UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 24.0 : 13.0];
         _subtitleLabel.numberOfLines = 0;
         _subtitleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
         _subtitleLabel.textAlignment = NSTextAlignmentLeft;
@@ -92,7 +69,7 @@
 
 - (UIImageView *)cellImageView {
     if (!_cellImageView) {
-        _cellImageView = [[UIImageView alloc] initWithFrame:CGRectMake(6.0f, 13.0f, 80.0f, 65.0f)];
+        _cellImageView = [[UIImageView alloc] init];
         _cellImageView.backgroundColor = [UIColor redColor];
         _cellImageView.contentMode = UIViewContentModeScaleToFill;
         _cellImageView.translatesAutoresizingMaskIntoConstraints = NO;
